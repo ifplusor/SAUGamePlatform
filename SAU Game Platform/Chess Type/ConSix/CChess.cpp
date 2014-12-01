7f31ecd0e009ac127d81b8c768f6496a3bbc557a
james <ywhjames@hotmail.com>
2014-12-01 15:25:05 +0800

    移除：移除CChess中的gameset字段和_GameSet结构，去除棋种支持模块对平台设置的依赖项。 增添：在CChess基类中增添LibPath字段，用以指明棋种支持模块所在的目录。 修改：将DrawAssist的第二个参数从d改为r，重新定义DrawAssist的实现方式。 重定义：重新定义了CChess基类存在的价值，将其对具体棋种的指导作用最大化，去除非通用的宏定义，将DrawPoint、DrawAssist、CreateSimpleFont、InsideRect四个普通成员函数声明为静态成员。 重定义：重新定义了棋种支持模块在程序目录中的组织形式，将棋种支持模块具有的依赖项全部移送到ChessType文件夹下的子项中，使其结构集中化。重新制定了ConSix项目的输出目录。

--- SAU Game Platform/Chess Type/ConSix/CChess.cpp
+++ SAU Game Platform/Chess Type/ConSix/CChess.cpp
@@ -14,6 +14,38 @@ CChess::~CChess()
 
 }
 
+//画特定大小的点(圆形)
+bool CChess::DrawPoint(HDC hDC,int r,COLORREF ptColor,COLORREF bkColor)
+{	
+	for(int i=0;i<=2*r;i++)
+	{
+		for(int j=0;j<=2*r;j++)
+		{
+			if((i-r)*(i-r)+(j-r)*(j-r)<r*r)
+				SetPixel(hDC,i,j,ptColor);
+			else
+				SetPixel(hDC,i,j,bkColor);
+		}		
+	}
+	return true;
+}
+
+//绘制圆形辅助位图(做透明处理)
+bool CChess::DrawAssist(HDC hAssistDC,int r)
+{
+	for(int i=0;i<r*2;i++)
+	{
+		for(int j=0;j<r*2;j++)
+		{
+			if ((i - r)*(i - r) + (j - r)*(j - r)<r*r)//圆内
+				SetPixel(hAssistDC,i,j,RGB(255,255,255));//白色
+			else//圆外
+				SetPixel(hAssistDC,i,j,RGB(0,0,0));//黑色
+		}
+	}
+	return true;
+}
+
 HFONT CChess::CreateSimpleFont(int width,int height)//创建一般字体
 {
 	HFONT hFont;
@@ -36,63 +68,10 @@ HFONT CChess::CreateSimpleFont(int width,int height)//创建一般字体
 	return hFont;
 }
 
-//画特定大小的点(圆形)
-bool CChess::DrawPoint(HDC hDC,int r,COLORREF ptColor,COLORREF bkColor)
-{	
-	for(int i=0;i<=2*r;i++)
-	{
-		for(int j=0;j<=2*r;j++)
-		{
-			if((i-r)*(i-r)+(j-r)*(j-r)<r*r)
-			{
-				SetPixel(hDC,i,j,ptColor);
-			}
-			else
-			{
-				SetPixel(hDC,i,j,bkColor);
-			}
-		}		
-	}
-	return true;
-}
-
 bool CChess::InsideRect(const RECT* rt,const int &x,const int &y)//判断点是否在矩形内
 {
 	if((rt->left+ALLOW)<x && x<(rt->right-ALLOW) && (rt->top+ALLOW)<y && y<(rt->bottom-ALLOW))
 		return true;
 	else
 		return false;
-}
-
-//绘制辅助位图(做透明处理)
-bool CChess::DrawAssist(HDC hAssistDC,int d)
-{
-	for(int i=0;i<d;i++)
-	{
-		for(int j=0;j<d;j++)
-		{
-			if(pow((double)(i-d/2),2)+pow((double)(j-d/2),2)<=pow((double)d/2,2))
-			{
-				SetPixel(hAssistDC,i,j,RGB(255,255,255));
-			}
-			else
-			{
-				SetPixel(hAssistDC,i,j,RGB(0,0,0));
-			}
-		}
-	}
-	return true;
-}
-
-//设置文本框文本内容
-VOID SetText(HWND hWnd,LPCTSTR str)
-{
-	SendMessage(hWnd,WM_SETTEXT,(WPARAM)0,(LPARAM)str);	//添加内容
-	SendMessage(hWnd,EM_SETSEL,(WPARAM)-2,(LPARAM)-1);	//设置焦点到末尾
-	SendMessage(hWnd,WM_VSCROLL,SB_BOTTOM,0);
-}
-
-VOID GetText(HWND hWnd,LPCTSTR str,int size)//获取文本框文本内容
-{	
-	SendMessage(hWnd,WM_GETTEXT,size,(LPARAM)str);
 }
--- SAU Game Platform/Chess Type/ConSix/CChess.h
+++ SAU Game Platform/Chess Type/ConSix/CChess.h
@@ -4,20 +4,16 @@
 #include <Windows.h>
 #include <math.h>
 
-#define BLACK 0//黑
-#define WHITE 1//白
-#define EMPTY 2//空
-#define	BAR 3//障碍
-#define UPLEFT 0//左上角
-#define DOWNLEFT 1//左下角
-#define CENTER 2//中心
+
 #define ALLOW 5
 #define BUFSIZE (1024*512)//管道大小512K
 
 #define GM_SHOWSTEP		0X1012
 #define GM_WINLOSE		0X1013
 
-#define NEXTPLAYER(S) (1-S)
+#define BLACK 0//黑方
+#define WHITE 1//白方
+#define NEXTPLAYER(S) (1-S)//轮换行棋权
 
 
 typedef struct
@@ -35,44 +31,13 @@ typedef struct
 	char* GameDate;//比赛结束时间
 }GAMEINFO;
 
-//程序设置
-typedef struct _GameSet{
-	//路径设置
-	char CurDir[MAX_PATH-100];//当前应用程序的目录
-	char BoardFileName[MAX_PATH];//棋盘材质路径
-	char BkPicFileName[MAX_PATH];//背景图片路径
-	char BlcPerFileName[MAX_PATH];//黑方人物图片路径
-	char WhtPerFileName[MAX_PATH];//白方人物图片路径
-	char BkMusicFileName[MAX_PATH];//背景音乐路径
-	char cmDir[MAX_PATH-100];//棋谱保存路径
-	char PrintScrDir[MAX_PATH-100];//截图路径
-	char cmFileName[MAX_PATH];//棋谱文件路径
-	char EngineInitDir[MAX_PATH-100];//引擎初始目录
-
-	//平台设置项标志
-	bool fullScr;//全屏 标志
-	bool SingleStep;//单步保存棋谱 标志
-	bool AllStep;//一起保存棋谱 标志
-	bool PrintScr;//截图 标志
-	bool CirclePlay;//循环播放背景音乐 标志
-	bool StopPlay;//停止播放背景音乐 标志
-	bool swEngine;//显示引擎信息 标志
-	int indexBdPic;//棋盘材质
-	int indexBkPic;//背景图片
-	int indexBlcPer;//人物图片
-	int indexWhtPer;
-	int BasePt;//棋盘的原点
-}GameSet;
-
-VOID SetText(HWND hWnd,LPCTSTR str);
-VOID GetText(HWND hWnd,LPCTSTR str,int size);
 
 class CChess
 {
 public:	
 	HWND hWnd;//窗口句柄
 
-	GameSet *gameset;//设置
+	char LibPath[MAX_PATH];//棋种支持模块路径
 
 	char curCmd[256];//待写入本方引擎命令
 	char denCmd[256];//待写入对方引擎命令
@@ -97,20 +62,18 @@ public:
 	//取消着法
 	virtual VOID CancelMove() = 0;
 
+	//绘制点（圆形）
+	static bool DrawPoint(HDC hDC,int r,COLORREF ptColor,COLORREF bkColor);	
+	//绘制圆形辅助位图
+	static bool DrawAssist(HDC hAssistDC,int r);
 	//创建简单字体
-	HFONT CreateSimpleFont(int width,int height);
-	//绘制点
-	bool DrawPoint(HDC hDC,int r,COLORREF ptColor,COLORREF bkColor);	
-	//绘制辅助位图
-	bool DrawAssist(HDC hAssistDC,int d);
+	static HFONT CreateSimpleFont(int width,int height);
 	//判断点是否在区域内
-	bool InsideRect(const RECT* rt,const int &x,const int &y);
+	static bool InsideRect(const RECT* rt,const int &x,const int &y);
 
 protected:
-
 	RECT rtBoard;//棋盘位置
 	int side;//棋盘宽度
-	int BasePt;//坐标原点标志
 	int StepNum[2];//黑白双方的步数
 	int count;//鼠标点击输入状态，-1为不接受鼠标点击输入
 };
